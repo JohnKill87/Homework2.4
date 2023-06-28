34983858403c1edcfb96f1a2878438f6b9b2785e
JohnKill87 <yankin.ilja2012@yandex.ru>
2023-06-28 17:26:39 +0300

    Решение Домашнего задания 2.4(Работа с исключениями)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import exceptions.WrongLoginException;
+import exceptions.WrongPasswordException;
+
 public class Main {
+    public static void Security(String login, String password, String confirmPassword) {
+        int maxSymbols = 20;
+        if (login.length() > maxSymbols || login.contains("Пароль") || login != "Wizardry_8") {
+            throw new WrongLoginException();
+        } if (password.length() > maxSymbols || password.contains("Пароль") || password != "Left_4_Dead_2") {
+            throw new WrongPasswordException();
+        }  if (password.equals(confirmPassword) == false) {
+            throw new WrongPasswordException();
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            Security("Wizardry_8", "Left_4_Dead_2", "Left_4_Dead_2");
+        } catch (WrongLoginException e) {
+            throw new WrongLoginException();
+        } catch (WrongPasswordException e) {
+            throw new WrongPasswordException();
+        }
     }
 }
