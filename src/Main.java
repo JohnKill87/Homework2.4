6c46170a9b0ee7da5aa41bd097dde335c74cc278
JohnKill87 <yankin.ilja2012@yandex.ru>
2023-06-30 17:04:22 +0300

    Решение Домашнего задания 2.4(Работа с исключениями)

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,17 @@ import exceptions.WrongPasswordException;
 public class Main {
     public static void Security(String login, String password, String confirmPassword) throws WrongLoginException, WrongPasswordException {
         int maxSymbols = 20;
-        String unacceptableSymbols ="^[a-zA-Z0-9-_]+$";
+        String validCharacters ="^[a-zA-Z0-9-_]+$";
         if (login.length() > maxSymbols) {
             throw new WrongLoginException();
-        } else if (!login.matches(unacceptableSymbols)){
+        } else if (!login.matches(validCharacters)){
             throw new WrongLoginException();
         }
 
 
         if (password.length() > maxSymbols) {
             throw new WrongPasswordException();
-        } else if (!password.matches(unacceptableSymbols)) {
+        } else if (!password.matches(validCharacters)) {
             throw new WrongPasswordException();
         }
 
